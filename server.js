435490e4283645bf5cb1034a0e748e12014ae177
Ignatius Sani <igee54077@gmail.com>
2024-10-20 13:24:46 +0100

    Initial commit

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ app.get('/config', (req, res) => {
 });
 
 // Start Server
-const port = 3000;
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
